3986e60d494f766424a7e1cbf47a8c4cf2d9327c
Joey <nguyenquocdat.vn@gmail.com>
2021-05-04 15:45:35 +0700

    toast transition

--- src/components/toasts/Toasts.js
+++ src/components/toasts/Toasts.js
@@ -1,52 +1,59 @@
 import tw, { styled } from 'twin.macro'
 import { useToaster } from 'react-hot-toast'
-import { Transition } from '@headlessui/react'
 import { variantIcons } from '../icon'
 import styledMap from 'styled-map'
 
 const ToastMessage = styled.div`
+  ${tw`transition-all ease-in-out duration-300
+    py-4 px-6 rounded-md flex items-center max-w-sm
+    shadow-xl bg-white ring-4 text-sm border
+    absolute w-full
+  `}
+  
   ${styledMap('type', {
-    success: tw`ring-positive`,
-    error: tw`ring-negative`,
-    default: tw`ring-primary`
+    success: tw`ring-positive border-positive`,
+    error: tw`ring-negative border-negative`,
+    default: tw`ring-primary border-primary`,
   })}
 
-  ${tw`py-4 px-6 rounded-md flex items-center max-w-sm
-    shadow-xl bg-white ring-opacity-10 ring-4 text-sm
-  `}
+  ${styledMap({
+    visible: tw`opacity-100`,
+    default: tw`opacity-0`
+  })}
+
+  ${tw`ring-opacity-10 border-opacity-40`}
 `
 
 const Toasts = () => {
-  const { visibleToasts, handlers } = useToaster()
-  const { startPause, endPause } = handlers
+  const { handlers, toasts } = useToaster()
+  const { startPause, endPause, calculateOffset } = handlers
 
   return (
     <div
-      tw='fixed top-8 left-1/2 -translate-x-1/2 transform max-w-sm w-full z-20'
+      tw='transition-transform fixed top-8 left-1/2 -translate-x-1/2 transform max-w-sm w-full z-20'
       onMouseEnter={startPause}
       onMouseLeave={endPause}
     >
-      {visibleToasts.map(toast => (
-        <Transition
-          key={toast.id}
-          show={toast.visible}
-          enter='transition-opacity'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='transition-opacity'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
-        >
+      {toasts.reverse().map((toast) => {
+        const offset = calculateOffset(toast.id, {
+          reverseOrder: true,
+          margin: 16,
+        })
+
+        return (
           <ToastMessage
+            key={toast.id}
             role={toast.role}
             aria-live={toast.ariaLive}
             type={toast.type}
+            style={{ transform: `translateY(${offset}px)` }}
+            visible={toast.visible}
           >
             {variantIcons[toast.type]}
             <div tw='line-clamp-3 flex-grow'>{toast.message}</div>
           </ToastMessage>
-        </Transition>
-      ))}
+        )
+      })}
     </div>
   )
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,5 @@
 import tw from 'twin.macro'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useQuery } from 'react-query'
 import useAPI from '../api/useAPI'
 import Layout from '../layouts/DefaultLayout'
@@ -8,9 +8,14 @@ import Link from '../components/link'
 import Table, { Column } from '../components/table'
 import { SelectionColumn } from '../components/table-plugins/selection'
 import { SortableColumn } from '../components/table-plugins/sortable'
+import { useDialog } from '../components/modal'
+import Button from '../components/button'
+import toast from 'react-hot-toast'
 
 const Home = ({ title }) => {
   const { sample } = useAPI()
+
+  const { confirm } = useDialog()
 
   const { data } = useQuery('users', sample.get)
 
@@ -18,10 +23,35 @@ const Home = ({ title }) => {
     selected: ['1'],
   })
 
+  const dialog = async () => {
+    const result = await confirm(
+      'This confirmation message open after page rendered.',
+      {
+        title: 'Are you done yet?',
+        variant: 'success',
+      },
+    )
+    toast('You clicked ' + (result ? 'OK' : 'Cancel'), {
+      type: result ? 'success' : 'error',
+    })
+  }
+
+  const openToast = () => {
+    toast('You opened a toast')
+  }
+
   return (
     <Layout>
       <PageHeader title={title} />
-      <section tw='mx-8 h-full'>
+      <section tw='m-8 h-full'>
+        <div tw='mb-8'>
+          <Button type='button' onClick={dialog}>
+            Sample Dialog
+          </Button>
+          <Button type='button' onClick={openToast}>
+            Sample Toast
+          </Button>
+        </div>
         <Table
           data={data}
           rowKey='id'
